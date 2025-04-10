0b179d1f75a370b61518d3f44d6598ada5485885
pankaj kumar singh <psingh227003@gmail.com>
2025-04-10 20:07:09 +0530

    arrys

---
+++ arrays.js
@@ -0,0 +1,9 @@
+//* ARRAYS IN JAVASCRIPT
+//* =====================================
+
+//! Iterable - object where you can use the for-of loop
+//! Array-like object - Any object with length property and use indexes to access items
+//! Arrays as Objects:  Arrays in JavaScript are a specific type of object that has numeric keys (indices) and a length property. The indices are automatically maintained, and the length property is automatically updated when you add or remove elements from the array.
+//! typeof Operator: The typeof operator in JavaScript returns "object" for both arrays and regular objects.
+
+//* JavaScript Array is a data structure that allows you to store and organize multiple values within a single variable. It is a versatile and dynamic object. It can hold various data types, including numbers, strings, objects, and even other arrays. Arrays in JavaScript are zero-indexed i.e. the first element is accessed with an index 0, the second element with an index of 1, and so forth.
