2ddbea8ef42d3e66f4d12e93479471d251a567a6
pankaj kumar singh <psingh227003@gmail.com>
2025-04-10 20:11:27 +0530

    arrays

--- arrays.js
+++ arrays.js
@@ -7,3 +7,115 @@
 //! typeof Operator: The typeof operator in JavaScript returns "object" for both arrays and regular objects.
 
 //* JavaScript Array is a data structure that allows you to store and organize multiple values within a single variable. It is a versatile and dynamic object. It can hold various data types, including numbers, strings, objects, and even other arrays. Arrays in JavaScript are zero-indexed i.e. the first element is accessed with an index 0, the second element with an index of 1, and so forth.
+
+//*  Creating Arrays:
+//* =====================================
+
+//? Arrays in JavaScript can be created using the Array constructor or with array literals (square brackets []).
+
+//? Using Array constructor
+// let fruits = new Array('apple', 'orange', 'banana')
+
+//? Using array literal
+// let fruits = ["apple", "orange", "banana"];
+// console.log(fruits);
+
+//? we can also create an empty array
+// let arr = [];
+// console.log(typeof arr);
+
+//* ======================================
+//*  Accessing Elements:
+//* =====================================
+//?👉 Accessing Elements:  Array elements are accessed using zero-based indices.
+// let fruits = ["apple", "orange", "banana"];
+// console.log(fruits[3]);
+// console.log(fruits["apple"]);
+
+//* ======================================
+//*  Modifying Elements:
+//* =====================================
+//?👉  Modifying Elements: You can modify array elements by assigning new values to specific indices.
+
+// let fruits = ["apple", "orange", "banana"];
+// fruits[2] = "mango";
+// console.log(fruits);
+
+//* =============================================
+//*  Array Traversal / Iterating Over Arrays
+//* ============================================
+//?👉 Array Traversal / Iterating Over Arrays
+
+// let fruits = ["apple", "orange", "mango", "grapes", "banana"];
+
+//? 1: for of loop , also known as iterable
+//* for...of Loop: The for...of loop is used to iterate over the values of an iterable object, such as arrays, strings, or other iterable objects.
+
+// for (let item of fruits) {
+//   console.log(item);
+// }
+
+// for (let item = 0; item < fruits.length; item++) {
+//   console.log(fruits[item]);
+// }
+
+//? 2: for in loop
+//* for...in Loop: The for...in loop is used to iterate over the properties (including indices) of an object.
+
+// for (let item in fruits) {
+//   console.log(item);
+// }
+
+// ? 3: forEach Method
+//* The arr.forEach() method calls the provided function once for each element of the array. The provided function may perform any kind of operation on the elements of the given array.
+
+// const myForEachArr = fruits.forEach((curElem, index, arr) => {
+//   return `${curElem} ${index}`;
+//   //   console.log(arr);
+// });
+// console.log(myForEachArr);
+
+// ? 4: map function
+//* map() creates a new array from calling a function for every array element. map() does not change the original array.
+
+// const myMapArr = fruits.map((curElem, index, arr) => {
+//   return ` my fav fruit is ${curElem} `;
+//   //   console.log(arr);
+// });
+
+// console.log(myMapArr);
+// console.log(fruits);
+
+//todo Practice Time
+//! write a program to Multiply each element with 2
+// const numbers = [1, 2, 3, 4, 5];
+// // forEach -  Performs an action on each element
+// // map -  Creates a new array with transformed elements
+
+// numbers.forEach((curElem) => {
+//   console.log(curElem * 2);
+//   //   Performs an action on each element
+// });
+
+// const doubleValue = numbers.map((curElem) => {
+//   return curElem * 2;
+//   //   Creates a new array with transformed elements
+// });
+
+// console.log(doubleValue);
+
+//* Key Differences
+//! Return Value:
+//? forEach: It doesn't return a value. The forEach method is used for iterating over the elements of an array and performing a side effect, such as modifying the array or performing a task for each element.
+
+//? map: It returns a new array containing the results of applying a function to each element in the original array. The original array remains unchanged.
+
+//! Chaining:
+//? forEach: It doesn't return a value, so it cannot be directly chained with other array methods.
+
+//? map: Since it returns a new array, you can chain other array methods after using map.
+
+//* Use Case:
+//? forEach: Used when you want to iterate over the array elements and perform an action on each element, but you don't need a new array.
+
+//? map: Used when you want to create a new array based on the transformation of each element in the original array.
