2e8a36f2282a5ab27a16950a43352c6c1378cbeb
pankaj kumar singh <psingh227003@gmail.com>
2025-04-10 20:03:30 +0530

    functions

--- function.js
+++ function.js
@@ -208,3 +208,42 @@
 //   }
 // };
 
+
+// console.log(calculator(5, 2, "+")); // Output: 7
+// console.log(calculator(8, 4, "-")); // Output: 4
+// console.log(calculator(10, 0, "/")); // Output: 5
+
+//! Reverse a String:
+//! Write a function to reverse a given string without using built-in reverse methods.
+
+// const isReverse = (str) => {
+//   let reverse = "";
+//   for (let char = str.length - 1; char >= 0; char--) {
+//     reverse = reverse + str[char];
+//   }
+//   return reverse;
+// };
+
+// console.log(isReverse("vinod thapa"));
+
+//! Palindrome Check:
+//! Create a function to determine if a given string is a palindrome (reads the same backward as forward).
+
+// // // isi     isi
+// // radar
+// // level
+// const isPalindrome = (str) => {
+//   let reverse = "";
+//   for (let char = str.length - 1; char >= 0; char--) {
+//     reverse = reverse + str[char];
+//   }
+//   //   if (str === reverse) {
+//   //     return true;
+//   //   } else {
+//   //     return false;
+//   //   }
+
+//   return str === reverse ? true : false;
+// };
+
+// console.log(isPalindrome("level"));
